586c29f94473443aed7bf8e38eb61602fd665e69
etherfal <lapodifahdan@gmail.com>
2026-09-20 16:13:27 +0900

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,25 @@
+// DAFTAR PLAYLIST LAGU DREAM AREA | 01
+// Kamu bisa bebas tambah/ubah lagu di sini tanpa ngotak-ngatik index.html!
+
+const playlist = [
+    {
+        title: "Lagu kebangsaan",
+        artist: "Dream Area 01",
+        src: "musik/lagu1.mp3"
+    },
+    {
+        title: "Play2",
+        artist: "Massage X",
+        src: "musik/lagu2.mp3"
+    },
+    {
+        title: "Play3",
+        artist: "Danza Kuduro",
+        src: "musik/lagu3.mp3"
+    }, 
+    {
+    	title: "Play4", 
+        artist: "Teh Hijau", 
+        src: " musik/lagu4.mp3"
+    }, 
+];
